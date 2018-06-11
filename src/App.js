98dfa637e58805b832bb8ac965d2ffc7d667c4ce
Johan Walles <johan.walles@gmail.com>
2018-06-11 18:28:10 +0200

    Add placeholder for making a move

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import React, { Component } from 'react';
 
 import Board from './components/Board';
-import BoardModel from './model/BoardModel';
 import logo from './logo.svg';
 
 class App extends Component {
@@ -17,7 +16,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Board board={new BoardModel(['x o', ' xo', '   '])}/>
+        <Board />
       </div>
     );
   }
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 
+import BoardModel from '../model/BoardModel';
+
 class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+       board: new BoardModel(['   ', '   ', '   '])
+    };
+
+    window.setInterval(this.makeMove, 1000);
+  }
+
   render() {
     return (
       <pre>{`
          ---
-        |${this.props.board.rows[0]}|
-        |${this.props.board.rows[1]}|
-        |${this.props.board.rows[2]}|
+        |${this.state.board.rows[0]}|
+        |${this.state.board.rows[1]}|
+        |${this.state.board.rows[2]}|
          ---
       `}</pre>
     );
+  }
+
+  makeMove() {
+    // FIXME: Make move here
+    console.log("Should have made a move");
   }
 }
 
